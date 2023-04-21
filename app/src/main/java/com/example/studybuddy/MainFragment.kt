56d292c8b366e68b7ae8fc540b561f3578c79860
singha24 <avijitsingh704@gmail.com>
2023-04-21 13:02:19 -0400

    AddFragment(4/21)

--- app/src/main/java/com/example/studybuddy/MainFragment.kt
+++ app/src/main/java/com/example/studybuddy/MainFragment.kt
@@ -1,22 +1,56 @@
 package com.example.studybuddy
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.studybuddy.databinding.FragmentMainBinding
 
-private var _binding : FragmentMainBinding? = null
-private val binding get() =_binding!!
-private val tasks = listOf(Task("Do the Dishes", listOf(4,30), listOf("Saturday","Sunday")))
 class MainFragment : Fragment() {
+    private var _binding : FragmentMainBinding? = null
+    private val binding get() =_binding!!
+    private val tasks = listOf(Task("Do the Dishes", listOf(4,30), listOf("Saturday","Sunday")),Task("Take out the Trash", listOf(8,0), listOf("Thursday")))
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentMainBinding.inflate(inflater,container,false)
         val rootView = binding.root
+
+        setHasOptionsMenu(true)
+
         val mAdapter = TaskAdapter(tasks)
         binding.recyclerview.adapter = mAdapter
 
+        binding.profile.setOnClickListener{
+            val action = MainFragmentDirections.actionMainFragmentToProfileFragment()
+            binding.root.findNavController().navigate(action)
+        }
+        binding.study.setOnClickListener{
+            val action = MainFragmentDirections.actionMainFragmentToStudyFragment()
+            binding.root.findNavController().navigate(action)
+        }
+        binding.add.setOnClickListener{
+            val action = MainFragmentDirections.actionMainFragmentToAddFragment()
+            binding.root.findNavController().navigate(action)
+        }
+        binding.settings.setOnClickListener{
+            val action = MainFragmentDirections.actionMainFragmentToSettingsFragment()
+            binding.root.findNavController().navigate(action)
+        }
+        binding.achivement.setOnClickListener{
+            val action = MainFragmentDirections.actionMainFragmentToAchievementFragment()
+            binding.root.findNavController().navigate(action)
+        }
         return rootView
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item,requireView().findNavController())
+                || super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/example/studybuddy/TaskViewHolder.kt
+++ app/src/main/java/com/example/studybuddy/TaskViewHolder.kt
@@ -15,15 +15,18 @@ class TaskViewHolder(val binding : ListItemLayoutBinding) : RecyclerView.ViewHol
     fun bindTask (task: Task){
         currentTask = task
         binding.task.text = currentTask.task
-        val time = "${currentTask.time[0]}: ${currentTask.time[1]}"
+        val time = "${currentTask.time[0]}: ${if(currentTask.time[1]<10){"0${currentTask.time[1]}"}else{currentTask.time[1]}}"
         binding.time.text = time
         var days = ""
+        if(currentTask.days.size!=1)
         for(day in currentTask.days){
             if(day != currentTask.days[currentTask.days.size-1])
                 days += "$day, "
             else
                 days += "& $day"
         }
+        else
+            days = currentTask.days[0]
         binding.days.text = days
     }
 }
